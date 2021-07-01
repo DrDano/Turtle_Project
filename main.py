be961669ddff3fbc894050555858cbc067bd3435
DrDano <danielharned@gmail.com>
2021-07-01 16:30:19 -0700

    070121 Commit of Turtle Project

--- main.py
+++ main.py
@@ -11,4 +11,11 @@
 # print(my_screen.canvheight)
 # my_screen.exitonclick()
 
-import prettytable
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "l"
+table.horizontal_char = "~"
+print(table.get_string(sortby="Pokemon Name", reversesort=False))
