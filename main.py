42864ab7fb859abecb2186c9aaf39af426e340bf
DrDano <danielharned@gmail.com>
2021-06-30 20:14:56 -0700

    First Commit of the Turtle Project to the Turtle Project repository.

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-
 # This is where we left off of the Turtle project
+# from turtle import Turtle, Screen
+#
+# ted = Turtle()
+# ted.shape("turtle")
+# ted.color("PaleTurquoise1")
+# ted.pencolor("red")
+# ted.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+import prettytable
